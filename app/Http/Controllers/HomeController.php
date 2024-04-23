08de04ffaa454575e83eb17114a451837ee5aced
Laes Uddin <laesuddin@gmail.com>
2024-04-23 22:26:37 +0600

    Showing Product in Main Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function rootpage(){
-        return view('home.userpage');
+        $products = product::all();
+        return view('home.userpage', compact('products'));
     }
     
     public function index(){
