d4c43c4bc8a40d393e6cd8f5764be28a8d279591
Laes Uddin <laesuddin@gmail.com>
2024-08-01 22:21:15 +0600

    Some code change

--- resources/views/home/showorder.blade.php
+++ resources/views/home/showorder.blade.php
@@ -10,8 +10,8 @@
       <meta name="keywords" content="" />
       <meta name="description" content="" />
       <meta name="author" content="" />
-      <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" type="">
-      <title>Famms - Fashion HTML Template</title>
+      <link rel="shortcut icon" href="{{asset('images/logo-mini.png')}}" type="">
+      <title>eCommerce</title>
       <!-- bootstrap core css -->
       <link rel="stylesheet" type="text/css" href="{{asset('home/css/bootstrap.css')}}"/>
       <!-- font awesome style -->
